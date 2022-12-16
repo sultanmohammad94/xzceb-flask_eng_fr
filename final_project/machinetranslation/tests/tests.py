1b2d3f1f2d3474d345f4c0cd1e68667fc2bcfa91
Mohammad Khair Sultan <83504044+sultanmohammad94@users.noreply.github.com>
2022-12-16 13:29:58 -0500

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -2,16 +2,13 @@ from unittest import TestCase
 from translator import french_to_english, english_to_french
 
 class TestIBMTranslator(TestCase):
-    def test_null_input(self):
-        self.assertIsNone(english_to_french(''))
-        self.assertIsNone(french_to_english(''))
-
-    def test_translation(self):
-        self.assertEqual(
-            english_to_french('Hello'),
-            'Bonjour'
-        )
-        self.assertEqual(
-            french_to_english('Bonjour'),
-            'Hello'
-        )
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english('Bonjour').lower(), 'Hello'.lower())
+        self.assertEqual(french_to_english('Bonsoir').lower(), 'Good evening'.lower())
+        self.assertNotEqual(french_to_english('Bonjour').lower(), ' '.lower())
+        
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french('Hello').lower(), 'Bonjour'.lower())
+        self.assertEqual(english_to_french('Good evening').lower(), 'Bonsoir'.lower())
+        self.assertNotEqual(english_to_french('Hello').lower(), ''.lower())
+        
